0c86cdc13bd08948b671a1fbec1c61f01069ce93
Shivam Gaikwad <shivamgaikwad.6565@gmail.com>
2026-07-26 00:01:58 +0530

    feat: add PATCH endpoint to update user profile with validation

--- src/app/api/profile/[username]/route.ts
+++ src/app/api/profile/[username]/route.ts
@@ -3,7 +3,7 @@ import { User } from "@/models/user.model";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/options";
 import ApiResponse from "@/types/ApiResponse";
-
+import {updateProfileSchema} from "@/schemas/updateProfile.schema";
 export async function GET(req: Request, { params }: { params: { username: string } }) {
   try {
     // 1. Authenticate user
@@ -37,4 +37,46 @@ export async function GET(req: Request, { params }: { params: { username: string
       { status: 500 }
     );
   }
+}
+
+export async function PATCH(req:Request, { params }: { params: { username: string } }) {
+    try{
+        const session = await getServerSession(authOptions);
+        if (!session?.user?._id) {
+            return NextResponse.json<ApiResponse>(
+                { success: false, message: "Unauthorized" },
+                { status: 401 }
+            );
+        }
+        const body = req.json();
+        const parsedBody = await updateProfileSchema.safeParse(body);
+        if(!parsedBody.success){
+            return NextResponse.json<ApiResponse>(
+                { success: false, message: "Invalid input", data: parsedBody.error.flatten() },
+                { status: 400 }
+            );
+        }
+         const updatedUser = User.findOneAndUpdate(
+            { _id: session.user._id },
+            parsedBody.data,
+            { new: true }
+         ).select("-passwordHash -verficationToken -ExpiresAt");
+         if(!updatedUser){
+            return NextResponse.json<ApiResponse>(
+                { success: false, message: "User not found" },
+                { status: 404 }
+            );
+         }
+
+         return NextResponse.json<ApiResponse>(
+            { success: true, message: "Profile updated successfully", data: updatedUser },
+            { status: 200 }
+         )
+    }catch(error){
+        console.error("Error updating user profile:", error);
+        return NextResponse.json<ApiResponse>(
+            { success: false, message: "Internal Server Error" },
+            { status: 500 }
+        );
+    }
 }
---
+++ src/schemas/updateProfile.schema.ts
@@ -0,0 +1,10 @@
+import {z} from "zod";
+
+export const updateProfileSchema = z.object({
+    username: z.string().min(3, "Username must be at least 3 characters long").max(20, "Username cannot exceed 20 characters").optional(),
+    name: z.string().min(1, "Name is required").max(50, "Name cannot exceed 50 characters").optional(),
+    bio: z.string().max(160, "Bio cannot exceed 160 characters").optional(),
+})
+
+export type UpdateProfileInput = z.input<typeof updateProfileSchema>;
+export type UpdateProfileOutput = z.output<typeof updateProfileSchema>;
