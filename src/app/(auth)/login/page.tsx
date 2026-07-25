b4ca39366a4e3f32b1826c5b1db412f644c78054
Shivam Gaikwad <shivamgaikwad.6565@gmail.com>
2026-07-25 19:43:41 +0530

    feat: implement login functionality with form validation and error handling

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,63 @@
-"use client"
-import { Eye, EyeOff } from 'lucide-react';
-import React from 'react'
-import { useState } from "react"
-import {useForm } from "react-hook-form"
-const page = () => {
+"use client";
+
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { Form, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import Image from "next/image";
+import { loginSchema, LoginSchemaInput, LoginSchemaOutput } from "@/schemas/login.schema";
+import Gradient from "../../../../public/Gradient.png";
+import { toast } from "sonner";
+import { Link } from "react-email";
+import { Eye, EyeOff } from "lucide-react";
+
+export default function SignIn() {
     const [showPassword, setShowPassword] = useState(false);
+    const router = useRouter();
+    const [error, setError] = useState<string | null>(null);
+
+    const form = useForm<LoginSchemaInput>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            identifier: "",
+            password: "",
+        },
+    });
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = form;
+
+    const onSubmit = async (data: LoginSchemaOutput) => {
+        try {
+            const result = await signIn("credentials", {
+                redirect: false,
+                identifier: data.identifier,
+                password: data.password,
+            });
+            if (result?.error) {
+                toast.error(result.error, { position: "top-right" });
+                setError(result.error);
+            } else if (result?.ok) {
+                toast.success("Login successful! Redirecting to dashboard...", {
+                    position: "top-right",
+                });
+                router.replace("/dashboard");
+            }
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                setError("Invalid email or password" + error.message);
+            } else {
+                setError("An unknown error occurred");
+            }
+        }
+    };
+
     return (
         < div
             className="bg-surface-container-low text-on-surface font-body-md min-h-screen flex flex-col"
@@ -15,7 +68,7 @@ const page = () => {
                 className="grow flex items-center justify-center w-full px-margin-mobile py-xl"
             >
                 <div
-                    className=" wil-[90%] md:w-[40%] bg-surface-container-lowest rounded-2xl shadow-md border border-outline-variant p-8 md:p-10"
+                    className=" w-[90%] md:w-[40%] bg-surface-container-lowest rounded-2xl shadow-md border border-outline-variant p-8 md:p-10"
                 >
 
                     <div className="mb-xl text-center">
@@ -40,7 +93,7 @@ const page = () => {
                         </p>
                     </div>
 
-                    <form action="#" className="space-y-6" method="POST">
+                    <form  className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
                         <div>
                             <label
                                 className="block font-label-md text-label-md text-on-surface mb-xs"
@@ -49,10 +102,12 @@ const page = () => {
                             <input
                                 className="w-full bg-surface-container-lowest border border-outline-variant rounded-lg px-4 py-3 font-body-md text-on-surface focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary transition-colors"
                                 id="email"
-                                name="email"
+
                                 placeholder="chef@example.com"
                                 type="text"
+                                {...register("identifier")}
                             />
+                            {errors.identifier && <p className="text-error text-sm mt-1">{errors.identifier.message}</p>}
                         </div>
                         <div>
                             <label
@@ -63,9 +118,10 @@ const page = () => {
                                 <input
                                     className="w-full bg-surface-container-lowest border border-outline-variant rounded-lg px-4 py-3 font-body-md text-on-surface focus:outline-none focus:border-primary focus:ring-1 focus:ring-primary transition-colors"
                                     id="password"
-                                    name="password"
+                                    
                                     placeholder="••••••••"
                                     type={showPassword ? "text" : "password"}
+                                    {...register("password")}
                                 />
                                 <button
                                     type="button"
@@ -74,6 +130,7 @@ const page = () => {
                                 >
                                     {showPassword ? <Eye /> : <EyeOff />}
                                 </button>
+                                {errors.password && <p className="text-error text-sm mt-1">{errors.password.message}</p>}
                             </div>
                         </div>
                         <div className="flex items-center justify-end">
@@ -87,6 +144,7 @@ const page = () => {
                         </div>
                         <div>
                             <button
+                                disabled={isSubmitting}
                                 className="w-full flex justify-center py-3 px-4 border border-transparent rounded-full shadow-sm font-label-md text-label-md text-on-primary bg-primary hover:bg-primary-container hover:text-on-primary-container focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary transition-colors"
                                 type="submit"
                             >
@@ -129,5 +187,3 @@ const page = () => {
         </div>
     )
 }
-
-export default page
