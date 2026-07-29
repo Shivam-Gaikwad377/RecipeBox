82eceb1e2ac5e32aa9ef4d90bb96a93c2951d802
Shivam Gaikwad <shivamgaikwad.6565@gmail.com>
2026-07-29 22:19:50 +0530

    feat(following): implement GET route to check follow status between users

---
+++ src/app/api/users/[id]/follow/[status]/route.ts
@@ -0,0 +1,33 @@
+//route to find the status is a user is following another
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../../auth/[...nextauth]/options";
+import { connectToDatabase } from "@/lib/dbConfig";
+import { Follow } from "@/models/following.model";
+import ApiResponse from "@/types/ApiResponse";
+
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
+    try{
+        const session = await getServerSession(authOptions);
+        const { id } = await params;
+        await connectToDatabase();
+        if(!session?.user?._id){
+            return NextResponse.json<ApiResponse>(
+                { success: false, message: "Unauthorized" },
+                { status: 401 }
+            );
+        }
+        const isFollowing = await Follow.exists({ follower: session.user._id, following: id });
+        return NextResponse.json<ApiResponse>(
+            { success: true, message: "Follow status fetched successfully", data: { isFollowing: !!isFollowing } },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error("Error fetching follow status:", error);
+        return NextResponse.json<ApiResponse>(
+            { success: false, message: "Internal Server Error" },
+            { status: 500 }
+        );
+        
+    }
+}
